a57273d3900aba1c8b527711be1773a22827aaa3
victor <vrubinr501@gmail.com>
2025-09-18 16:20:48 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Hola mundo");
+Console.WriteLine("Informaticonfig");
+Console.WriteLine(50 + 60);
+
+string persona = "Juan";
+int edad = 30;
+bool esDesarrollador = true;
+
+Console.WriteLine($"Nombre: {persona}, Edad: {edad}, Desarrollador: {esDesarrollador}");
+
